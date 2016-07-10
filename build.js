b3e263be504f1fd71cb925a28b8b5c8d68c5ec3b
Mark Pajak <markpajak.spider@gmail.com>
2016-07-10 16:40:43 +0100

    Add files via upload

---
+++ build.js
@@ -0,0 +1,34 @@
+({
+    appDir: './',
+    baseUrl: './js',
+    dir: './dist',
+    modules: [
+        {
+            name: 'main'
+        }
+    ],
+    fileExclusionRegExp: /^(r|build)\.js$/,
+    optimizeCss: 'standard',
+    removeCombined: true,
+    paths: {
+        jquery: 'libs/jquery/jquery-min',
+        underscore: 'libs/underscore/underscore-min',
+        backbone: 'libs/backbone/backbone-min',
+		fotorama_amd:'libs/fotorama/fotoramaAMD',
+		Globals:'helpers/Globals',
+        text: 'text'
+    },
+	 inlineText: false,
+    shim: {
+        underscore: {
+            exports: '_'
+        },
+        backbone: {
+            deps: [
+                'underscore',
+                'jquery'
+            ],
+            exports: 'Backbone'
+        }
+    }
+})
